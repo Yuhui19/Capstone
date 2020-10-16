726a0f8b810460456e94594ff36cfc910f9a19a7
xiangjieli <xiangjieli1997@gmail.com>
2020-10-16 10:54:37 -0600

    finish auth service

--- auth/main.go
+++ auth/main.go
@@ -15,9 +15,11 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	// "github.com/aws/aws-sdk-go/service/dynamodb/expression"
 
+	"github.com/dgrijalva/jwt-go"
 	// "encoding/json"
 	// "io/ioutil"
 	"os"
+	"time"
 )
 
 
@@ -27,13 +29,24 @@ type User struct {
 	Password string `json:"password" binding:"required"`
 }
 
+// Create the JWT key used to create the signature
+var jwtKey = []byte("my_secret_key")
+
+
+// Create a struct that will be encoded to a JWT.
+// We add jwt.StandardClaims as an embedded type, to provide fields like expiry time
+type Claims struct {
+	Email string `json:"email"`
+	jwt.StandardClaims
+}
+
 
 func main() {
 
 	mySession, err := session.NewSession(
 		&aws.Config{
 			Region: aws.String("us-east-1"),
-			Credentials: credentials.NewStaticCredentials("ASIAQOOYYKP45QWY75CW", "+i3lbnjfmdITyV0BQqdX3PM04zIG+qkJDHkkC0uh", "FwoGZXIvYXdzEL///////////wEaDIZo7wh7SfxnxvlRxiLAAc04B5WxwmVB/827HE826tm3X3auFqvJWIQZ+IiYa494N33ym9LcpPuDZez5wShAHI5j/GTTIXK+4ARC6V9cWyFpemHfegSa1gR3g03zzcNWm0Inx9g4ZyPet9ElxD96cAFGiQ6yh1jyZJMgWbe0XjOzKfixkgAMlNDtjs/jG6QOmA3pZXl1MLHGdffCdRE6nloZO9AYSB1Rg0jj5rO37MF4t8Scf6WDxX6S2oKKGCXw+zelQSmIt9btV+0P10CmLCiY5qT8BTItmmtV2kWIo5mjM2hyQm4egjlqeFUX2Ush6PEBVWf9HvGtUkFlGeoFpYBfS5dR"),
+			Credentials: credentials.NewStaticCredentials("ASIAQOOYYKP43UNQCIMY", "zktt1awrSyCcNQ7P26jmGuG6uN7jkszxg7vV3I/D", "FwoGZXIvYXdzEMn//////////wEaDJ+9O0hV0hMgHh5NWCLAAa2SU2Ll5mwnimlqUjOdsxXWmibzf9+wCMA+qzgUpzyyliC9QzF9a+qCZMwFDbP+vr1pplVC6Tq+/d7bsS5sorkfORbXVxUQzLK5lSH5mevK17LDxMmQ/uxexCPzgpZnAg2b3WDBqQqm8VF8klsabOn0OQ248WnxITBnin1AnA5RS27R+e9YTuaavxdujFer8C8urK+DaJdREKbcJnFjAdnkMlFKi8jzGj1FOQuhG0juDibtdtJaAdGdpKAxf8f1hyj5/ab8BTItJJw/6shwZh9eyOEyFBZNmoJf+Ap1ahR+KxJrTQsr5GaUMG2lBqr/Uh4YLyvO"),
 		},
 	)
 	if err != nil {
@@ -89,6 +102,34 @@ func main() {
 
 		// create a user in the database
 		createNewUser(svc, userJson.Email, hashedPasswordStr, tableName)
+
+
+		// Declare the expiration time of the token
+		// here, we have kept it as 5 minutes
+		expirationTime := time.Now().Add(5 * time.Minute)
+		// Create the JWT claims, which includes the username and expiry time
+		claims := &Claims{
+			Email: userJson.Email,
+			StandardClaims: jwt.StandardClaims{
+				// In JWT, the expiry time is expressed as unix milliseconds
+				ExpiresAt: expirationTime.Unix(),
+			},
+		}
+
+		// Declare the token with the algorithm used for signing, and the claims
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		// Create the JWT string
+		tokenString, err := token.SignedString(jwtKey)
+		if err != nil {
+			// If there is an error in creating the JWT return an internal server error
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "cannot create a jwt"})
+			return
+		}
+
+		// Finally, we set the client cookie for "token" as the JWT we just generated
+		// we also set an expiry time which is the same as the token itself
+		c.SetCookie("token", tokenString, int(expirationTime.Unix() * 1000), "/", "localhost", false, true)
+
 		
 		c.JSON(http.StatusOK, gin.H{
 			"result": "create a new user successfully!",
@@ -126,7 +167,33 @@ func main() {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "password doesn't match"})
 			return
 		}
-		
+
+		// Declare the expiration time of the token
+		// here, we have kept it as 5 minutes
+		expirationTime := time.Now().Add(5 * time.Minute)
+		// Create the JWT claims, which includes the username and expiry time
+		claims := &Claims{
+			Email: userJson.Email,
+			StandardClaims: jwt.StandardClaims{
+				// In JWT, the expiry time is expressed as unix milliseconds
+				ExpiresAt: expirationTime.Unix(),
+			},
+		}
+
+		// Declare the token with the algorithm used for signing, and the claims
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		// Create the JWT string
+		tokenString, err := token.SignedString(jwtKey)
+		if err != nil {
+			// If there is an error in creating the JWT return an internal server error
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "cannot create a jwt"})
+			return
+		}
+
+		// Finally, we set the client cookie for "token" as the JWT we just generated
+		// we also set an expiry time which is the same as the token itself
+		c.SetCookie("token", tokenString, int(expirationTime.Unix() * 1000), "/", "localhost", false, true)
+
 		c.JSON(http.StatusOK, gin.H{
 			"result": "you have logged in!",
 		})
@@ -135,13 +202,54 @@ func main() {
 
 
 
-	// router.POST("/api/users/signout", func(c *gin.Context) {
+	router.POST("/api/users/signout", func(c *gin.Context) {
+		c.SetCookie("token", "", 0, "/", "localhost", false, true)
+		c.JSON(http.StatusOK, gin.H{
+			"result": "you have logged out!",
+		})
+	})
 
-	// })
+	
+	router.GET("/api/users/currentuser", func(c *gin.Context) {
+		// We can obtain the session token from the requests cookies, which come with every request
+		cookie, err := c.Request.Cookie("token")
+        if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "no cookie, no jwt, please sign in"})
+			return
+        }
 
-	// router.GET("/api/users/currentuser", func(c *gin.Context) {
 
-	// })
+		// Get the JWT string from the cookie
+		tknStr := cookie.Value
+
+		// Initialize a new instance of `Claims`
+		claims := &Claims{}
+
+		// Parse the JWT string and store the result in `claims`.
+		// Note that we are passing the key in this method as well. This method will return an error
+		// if the token is invalid (if it has expired according to the expiry time we set on sign in),
+		// or if the signature does not match
+		tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		})
+		if err != nil {
+			if err == jwt.ErrSignatureInvalid {
+				c.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized user"})
+				return
+			}
+			c.JSON(http.StatusBadRequest, gin.H{"error": "errors when parsing jwt"})
+			return
+		}
+		if !tkn.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid jwt"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"email": claims.Email,
+		})
+
+	})
 
 	router.Run()
 	
